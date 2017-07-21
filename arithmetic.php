72da5f6db4113351bae51137b4b6eba9547f384e
Vivian Canales <viviancans@gmail.com>
2017-07-21 16:05:17 -0500

    added code to arthmetic functions. called functions with practice values

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,29 @@
 <?php
 
-function add($a, $b)
-{
-    return $a + $b;
-}
+	function add($a, $b)
+	{
+	    return $a + $b;
+	}
 
-function subtract($a, $b)
-{
-    // Add code here
-}
+	function subtract($a, $b)
+	{
+	    return $a - $b; 
+	}
 
-function multiply($a, $b)
-{
-    // Add code here
-}
+	function multiply($a, $b)
+	{
+		return $a * $b; 
+	}
 
-function divide($a, $b)
-{
-    // Add code here
-}
+	function divide($a, $b)
+	{
+	    return $a / $b; 
+	}
 
-// Add code to test your functions here
+	echo add(2, 7) . PHP_EOL;
+
+	echo subtract(10, 9) . PHP_EOL;
+
+	echo multiply(1, 7) . PHP_EOL;
+
+	echo divide(10, 2) .PHP_EOL;
