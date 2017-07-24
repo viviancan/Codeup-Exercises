ff59ebed0697040602958297b4ffb33cf538ab56
Vivian Canales <viviancans@gmail.com>
2017-07-24 16:52:02 -0500

    corrected search arrays exercise

--- search-arrays.php
+++ search-arrays.php
@@ -8,22 +8,51 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 //Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
 
 
-
-// $result = array_search('Tina', $names);
-// var_dump($result);
-
-
 	function ifValueExists($needle, $haystack)
 	{
 		if(array_search($needle, $haystack) === false){
-			return "false"; 		
+			return false; 		
 		} else {
 
-			return "true"; 		
+			return true; 		
 		}
 	}
 
-echo ifValueExists('Bob', $names) . PHP_EOL;
+
+	if(ifValueExists('Tina', $names)){
+		echo "Tina is in the array" . PHP_EOL;
+	}else {
+		echo "Tina is not in the array." . PHP_EOL;
+	}
+
+	if(ifValueExists('BOB', $names)){
+		echo "BOB is in the array" . PHP_EOL;
+	}else {
+		echo "BOB is not in the array." . PHP_EOL;
+	}
+
+
+
+
+// //Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
+
+
+function numberOfValuesInCommon($array1, $array2)
+{
+	$count = 0;
+
+	foreach($array1 as $value){
+		if(ifValueExists($value,$array2)){
+			$count++ ;
+		}	
+	}
+
+	return $count; 
+
+}
+
+
+echo numberOfValuesInCommon($names, $compare) . PHP_EOL; 
 
 
 
@@ -31,11 +60,17 @@ echo ifValueExists('Bob', $names) . PHP_EOL;
 
 
 
+	// function ifValueExists($needle, $haystack, $compare)
+	// {
 
+	// 	if(array_search($needle, $haystack) === false && array_search($needle, $compare === false)){
+	// 		return "false"; 		
+	// 	} 
 
+	// 	if(array_search($needle, $haystack) === true && array_search($needle, $compare === true)){
+	// 		return "true";
+	// 	}
+	// }
 
-
-
-
-
+	// echo ifValueExists('Tina', $names, $compare) . PHP_EOL;
 
