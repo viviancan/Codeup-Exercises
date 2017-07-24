c6791998895232d337ac6905e6740b55d719ca0f
Vivian Canales <viviancans@gmail.com>
2017-07-24 13:27:24 -0500

    completed array functions exercises

---
+++ internal _functions_bonus.php
@@ -0,0 +1,15 @@
+INTERNAL FUNCTIONS BONUS:
+
+	Create a function that takes in a string and echoes out how many occurences of each alpha-numeric character is present in the string.
+
+	Example:
+
+	The string "aabb131"
+
+	Should yield the following output:
+
+	This string contains...
+	- (2) "a" characters
+	- (2) "b" characters
+	- (2) "1" characters
+	- (1) "3" characters
