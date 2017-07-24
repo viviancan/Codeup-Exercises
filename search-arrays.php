7e167c4128b27ea255248f20be4ab4768cba748e
Vivian Canales <viviancans@gmail.com>
2017-07-24 16:02:01 -0500

    created function to look for specific value in array

--- search-arrays.php
+++ search-arrays.php
@@ -3,3 +3,39 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+
+//Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
+
+
+
+// $result = array_search('Tina', $names);
+// var_dump($result);
+
+
+	function ifValueExists($needle, $haystack)
+	{
+		if(array_search($needle, $haystack) === false){
+			return "false"; 		
+		} else {
+
+			return "true"; 		
+		}
+	}
+
+echo ifValueExists('Bob', $names) . PHP_EOL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
