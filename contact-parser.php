78bac73d6d1590021355b861412fdc2c264a737d
Vivian Canales <viviancans@gmail.com>
2017-07-26 11:38:42 -0500

    completed contact parser exercise.

--- contact-parser.php
+++ contact-parser.php
@@ -1,12 +1,38 @@
 <?php
 
+
 function parseContacts($filename)
 {
-    $contacts = array();
+	$results = array();
+	$handle = fopen($filename, 'r');
+	$contents = fread($handle, filesize($filename));
+	$contacts = explode("\n", $contents);
 
-    // todo - read file and parse contacts
+	foreach ($contacts as $key => $value) {
+		$contact = explode("|" , $value);
 
-    return $contacts;
+		$contact[1] = substr($contact[1], 0, 3) . "-" . substr($contact[1], 3, 3) . "-" . substr($contact[1], 6);
+
+		$assocArray["name"] = $contact[0];
+		$assocArray["number"] = $contact[1];
+		array_push($results, $assocArray);
+	}
+
+	fclose($handle);
+	return $results;
 }
 
+
 var_dump(parseContacts('contacts.txt'));
+
+
+
+
+
+
+
+
+
+
+
+
