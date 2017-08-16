361093f488393a75f3eaf37e76fa4ade0c8517a5
Vivian <vcnales@gmail.com>
2017-08-16 15:58:58 -0500

    magic methods & late static binding exercise

---
+++ Model.php
@@ -0,0 +1,48 @@
+<?php 
+
+
+class Model 
+{
+
+	protected static $table; 
+
+	private $data = []; 
+
+	public function __set($name, $value)
+	{
+		$this->data[$name] = $value; 
+	}
+
+	public function __get($name)
+	{
+
+		if(array_key_exists($name, $this->data)){
+
+			return $this->data[$name];
+		}
+		return null;
+
+	}
+
+	public static function getTableName()
+	{
+		return static::$table; 
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+ ?>
---
+++ model_test.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+require_once "Model.php";
+require_once "User.php";
+
+
+// $data = new Model();
+// $data->name = 'Arthur Dent'
+// $data->group = 'Codeup';
+// $data->age = 42
+
+// var_dump($data);
+
+echo User::getTableName();
+
+
+
+ ?>
